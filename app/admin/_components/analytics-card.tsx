d081d3f941629ce403fd45005e2a876d5c88aa0a
Piotr Kulpinski <piotr@kulpinski.pl>
2024-10-25 15:56:41 +0200

    clean up admin route structure

---
+++ app/admin/_components/analytics-card.tsx
@@ -0,0 +1,52 @@
+import ky from "ky"
+import type { ComponentProps } from "react"
+import { AnalyticsChart, type AnalyticsChartData } from "~/app/admin/_components/analytics-chart"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "~/components/admin/ui/card"
+import { Skeleton } from "~/components/common/skeleton"
+import { env } from "~/env"
+
+export const AnalyticsCard = async ({ ...props }: ComponentProps<typeof Card>) => {
+  const siteId = "openalternative.co"
+  const apiEndpoint = `${env.NEXT_PUBLIC_PLAUSIBLE_HOST}/api/v1/stats/timeseries?metrics=visitors&period=30d&site_id=${siteId}`
+
+  const { results } = await ky
+    .get(apiEndpoint, { headers: { Authorization: `Bearer ${env.PLAUSIBLE_API_KEY}` } })
+    .json<{ results: AnalyticsChartData[] }>()
+
+  const totalVisitors = results.reduce((acc, curr) => acc + curr.visitors, 0)
+  const averageVisitors = Math.round(totalVisitors / results.length)
+
+  return (
+    <Card {...props}>
+      <CardHeader>
+        <CardDescription>Visitors</CardDescription>
+        <CardTitle className="text-3xl">{totalVisitors.toLocaleString()}</CardTitle>
+      </CardHeader>
+
+      <CardContent>
+        <AnalyticsChart data={results} average={averageVisitors} className="h-56 w-full" />
+      </CardContent>
+    </Card>
+  )
+}
+
+export const AnalyticsCardSkeleton = ({ ...props }: ComponentProps<typeof Card>) => {
+  return (
+    <Card {...props}>
+      <CardHeader>
+        <Skeleton className="h-5 w-12" />
+        <Skeleton className="text-3xl w-24">&nbsp;</Skeleton>
+      </CardHeader>
+
+      <CardContent>
+        <Skeleton className="h-56 w-full" />
+      </CardContent>
+    </Card>
+  )
+}
---
+++ app/admin/tools/_components/tool-actions.tsx
@@ -0,0 +1,121 @@
+"use client"
+
+import type { Tool } from "@prisma/client"
+import type { Row } from "@tanstack/react-table"
+import { EllipsisIcon } from "lucide-react"
+import Link from "next/link"
+import { useRouter } from "next/navigation"
+import React from "react"
+import { toast } from "sonner"
+import { useServerAction } from "zsa-react"
+import { ToolsDeleteDialog } from "~/app/admin/tools/_components/tools-delete-dialog"
+import { ToolsScheduleDialog } from "~/app/admin/tools/_components/tools-schedule-dialog"
+import { reuploadToolAssets } from "~/app/admin/tools/_lib/actions"
+import { Button } from "~/components/admin/ui/button"
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "~/components/admin/ui/dropdown-menu"
+import { siteConfig } from "~/config/site"
+import { cx } from "~/utils/cva"
+
+interface ToolActionsProps extends React.ComponentPropsWithoutRef<typeof Button> {
+  tool: Tool
+  row?: Row<Tool>
+}
+
+export const ToolActions = ({ tool, row, className, ...props }: ToolActionsProps) => {
+  const router = useRouter()
+  const [showToolsDeleteDialog, setShowToolsDeleteDialog] = React.useState(false)
+  const [showToolsScheduleDialog, setShowToolsScheduleDialog] = React.useState(false)
+
+  const { execute: reuploadAssetsAction } = useServerAction(reuploadToolAssets, {
+    onSuccess: () => {
+      toast.success("Tool assets reuploaded")
+    },
+
+    onError: ({ err }) => {
+      toast.error(err.message)
+    },
+  })
+
+  return (
+    <>
+      <ToolsDeleteDialog
+        open={showToolsDeleteDialog}
+        onOpenChange={setShowToolsDeleteDialog}
+        tools={[tool]}
+        showTrigger={false}
+        onSuccess={() => row?.toggleSelected(false) || router.push("/admin/tools")}
+      />
+
+      <ToolsScheduleDialog
+        open={showToolsScheduleDialog}
+        onOpenChange={setShowToolsScheduleDialog}
+        tools={[tool]}
+        showTrigger={false}
+      />
+
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button
+            aria-label="Open menu"
+            variant="outline"
+            size="sm"
+            prefix={<EllipsisIcon />}
+            className={cx("size-7 data-[state=open]:bg-muted", className)}
+            {...props}
+          />
+        </DropdownMenuTrigger>
+
+        <DropdownMenuContent align="end">
+          <DropdownMenuItem asChild>
+            <Link href={`/admin/tools/${tool.slug}`}>Edit</Link>
+          </DropdownMenuItem>
+
+          {!tool.publishedAt && (
+            <DropdownMenuItem
+              onSelect={() => setShowToolsScheduleDialog(true)}
+              className="text-green-600 dark:text-green-400"
+            >
+              Schedule
+            </DropdownMenuItem>
+          )}
+
+          {tool.publishedAt && tool.publishedAt <= new Date() && (
+            <DropdownMenuItem asChild>
+              <Link href={`${siteConfig.url}/tools/${tool.slug}`} target="_blank">
+                View
+              </Link>
+            </DropdownMenuItem>
+          )}
+
+          <DropdownMenuItem onSelect={() => reuploadAssetsAction({ id: tool.id })}>
+            Reupload Assets
+          </DropdownMenuItem>
+
+          <DropdownMenuSeparator />
+
+          <DropdownMenuItem asChild>
+            <Link href={tool.websiteUrl} target="_blank">
+              Visit website
+            </Link>
+          </DropdownMenuItem>
+
+          <DropdownMenuSeparator />
+
+          <DropdownMenuItem
+            onSelect={() => setShowToolsDeleteDialog(true)}
+            className="text-red-500"
+          >
+            Delete
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
+  )
+}
