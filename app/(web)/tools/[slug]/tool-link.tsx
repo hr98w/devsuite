16f94f0b644a7134d2da87c904aec93b434546ac
Piotr Kulpinski <piotr@kulpinski.pl>
2024-11-08 09:39:35 +0100

    add posthog click tracking again

---
+++ app/(web)/tools/[slug]/tool-link.tsx
@@ -0,0 +1,32 @@
+"use client"
+
+import { getUrlHostname } from "@curiousleaf/utils"
+import { ArrowUpRightIcon } from "lucide-react"
+import { posthog } from "posthog-js"
+import type { ComponentProps } from "react"
+import { Button } from "~/components/web/ui/button"
+import type { ToolOne } from "~/server/tools/payloads"
+
+type ToolLinkProps = ComponentProps<typeof Button> & {
+  tool: ToolOne
+}
+
+export const ToolLink = ({ tool, ...props }: ToolLinkProps) => {
+  return (
+    <Button
+      suffix={<ArrowUpRightIcon />}
+      onClick={() => posthog.capture("tool_clicked", { url: tool.websiteUrl })}
+      asChild
+      {...props}
+    >
+      <a
+        href={tool.affiliateUrl || tool.websiteUrl}
+        target="_blank"
+        rel={`noreferrer noopener ${tool.isFeatured ? "" : "nofollow"}`}
+      >
+        <span className="sm:hidden">Visit</span>
+        <span className="max-sm:hidden">{getUrlHostname(tool.websiteUrl)}</span>
+      </a>
+    </Button>
+  )
+}
